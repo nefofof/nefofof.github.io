35f6ed73dd3e572d7c1b6d38bdd8095c56bc09f0
nefofof <84814706+nefofof@users.noreply.github.com>
2022-07-12 08:42:25 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "OIDRP";
+var l_serverName = "Oid RP";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
