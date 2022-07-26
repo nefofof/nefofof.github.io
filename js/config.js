ac40c4e6ce7ef3f76cf1066d013c0d61d837c1da
nefofof <84814706+nefofof@users.noreply.github.com>
2022-07-27 02:46:08 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -73,7 +73,7 @@ var l_bgOverlay = false;
 
 /*Background darkening amount
 0 for none (0%), 100 for pitch black (100%)*/
-var l_bgDarkening = 100;
+var l_bgDarkening = 90;
 
 /*Music player
 'true' or 'false' to enable music in the background*/
